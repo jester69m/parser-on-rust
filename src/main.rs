271c3dfed910399cf98635d81750275bb7d60429
jester_69m <pashchuknazar@ukr.net>
2023-11-14 20:57:45 +0200

    optimized code

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,6 @@ use std::error::Error;
 
 use parser_on_rust::parse_url;
 
-// fn main() {
-//     let url_string = "https://www.example.com";
-//     match parse_url(url_string) {
-//         Ok(parsed_url) => println!("Successfully parsed URL: {:?}", parsed_url),
-//         Err(err) => eprintln!("Error parsing URL: {}", err),
-//     }
-// }
-
 fn main() -> Result<(), Box<dyn Error>> {
     // Example URLs to parse
     let urls = [
